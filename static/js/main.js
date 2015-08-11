00d040f74aaf938534aff3318e16ba2d9c54239e
Grant Forrest <a-type@outlook.com>
2015-08-10 23:04:25 -0400

    Use a real shell session

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,27 @@
 "use strict";
 
-var $        = require("jquery");
-var terminal = require("./lib/terminal.js");
+var $       = require("jquery");
+var Command = require("./lib/command");
+var Shell   = require("./lib/shell");
 
 $(function () {
+	var shell = new Shell();
+
 	var $stdin = $("#stdin");
 	var $stdout = $("#stdout");
 	$stdin.keydown(function (event) {
 		if(event.keyCode == 13) {
-			var command = $stdin.val();
-			terminal.runCommand(command)
-			.then(function (result) {
-				$stdout.append(result.output);
-			});
+			var commandString = $stdin.val();
+			shell.runCommand(new Command(commandString));
+			$stdin.val("");
 		}
+	});
+
+	shell.on("out", function (data) {
+		$stdout.append("<div>" + data + "</div>");
+	});
+
+	shell.on("err", function (data) {
+		$stdout.append("[ERROR] " + data);
 	});
 });
