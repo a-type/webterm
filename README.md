da745098e3463d6fe3d686b68c2db47e301a5fe9
Grant Forrest <a-type@outlook.com>
2015-08-11 15:37:55 -0400

    Better styling and correct whitespace

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ webterm
 =======
 
 I don't know what I'm doing
+
+Install `electron-prebuilt` globally before running.
--- static/js/main.js
+++ static/js/main.js
@@ -3,25 +3,58 @@
 var $       = require("jquery");
 var Command = require("./lib/command");
 var Shell   = require("./lib/shell");
+var _       = require("lodash");
 
 $(function () {
 	var shell = new Shell();
 
+	var promptTpl = _.template($("#promptTemplate").text().trim());
+	var outputTpl = _.template($("#outputTemplate").text().trim());
+	var errorTpl = _.template($("#errorTemplate").text().trim());
+
 	var $stdin = $("#stdin");
 	var $stdout = $("#stdout");
-	$stdin.keydown(function (event) {
-		if(event.keyCode == 13) {
-			var commandString = $stdin.val();
-			shell.runCommand(new Command(commandString));
-			$stdin.val("");
+
+	function resetInput () {
+		$stdin.remove();
+		$stdout.append($stdin);
+		$stdin.keydown(function (event) {
+			$stdin.attr("size", $stdin.val().length + 1);
+			if(event.keyCode == 13) {
+				var commandString = $stdin.val();
+				shell.runCommand(new Command(commandString));
+				$stdin.val("").attr("size", 1).remove();
+			}
+		});
+		$stdin.focus();
+	}
+	resetInput();
+
+	var lastWasPrompt = false;
+
+	shell.on("out", function (data) {
+		var $generated;
+
+		switch(data.type) {
+			case "prompt":
+				lastWasPrompt = true;
+				$generated = $(promptTpl(data));
+				$stdout.append($generated);
+				resetInput();
+				break;
+			default:
+				$generated = $(outputTpl(data));
+				if (lastWasPrompt) {
+					$generated.addClass("command");
+				}
+				lastWasPrompt = false;
+				$stdout.append($generated);
+				break;
 		}
 	});
 
-	shell.on("out", function (data) {
-		$stdout.append("<div>" + data + "</div>");
-	});
-
 	shell.on("err", function (data) {
-		$stdout.append("[ERROR] " + data);
+		$stdout.append($(errorTpl(data)));
+		resetInput();
 	});
 });
