61436529a7ae933325b6b2aa7fb0ab47ef5feb35
Grant Forrest <a-type@outlook.com>
2015-08-22 17:49:59 -0400

    More shell extraction

--- static/js/main.js
+++ static/js/main.js
@@ -3,21 +3,22 @@
 var Terminal         = require("./lib/terminal");
 var TerminalDomInput = require("./lib/domInput");
 var TerminalRenderer = require("./lib/terminal/renderer");
-var Shell            = require("./lib/shell");
+var Shell            = require("jsh");
+var stream           = require("stream");
 var _                = require("lodash");
 
 document.addEventListener("DOMContentLoaded", function () {
 	var mainDisplay = document.getElementsByClassName("main-display")[0];
 
+	var shellInputStream = new TerminalDomInput();
+	shellInputStream.attach(mainDisplay);
+
 	var shell = new Shell();
 
-	var term = new Terminal({ rows: 24, columns: 80 });
-	var shellInput = new TerminalDomInput(mainDisplay, shell);
-	var termOutput = new TerminalRenderer(mainDisplay, term);
+	shellInputStream.pipe(shell);
 
-	shell.on("data", function (data) {
-		term.write(data);
-	});
+	var term = new Terminal(shell);
+	var termOutput = new TerminalRenderer(mainDisplay, term);
 
 	setTimeout(mainDisplay.focus(), 10);
 });
